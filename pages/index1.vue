e4a7956a11a46fc57fa4abf18ff1dea345404f9a
btm2021 <trinhminhbao@gmail.com>
2023-06-06 21:23:29 +0700

    asd

---
+++ pages/index1.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+<script>
+const { Deta } = require('deta'); // import Deta
+
+// Initialize with a Project Key
+// locally, set the project key in an env var called DETA_PROJECT_KEY
+const deta = Deta("c0zw9qrjkal_ehAKM7hG7k8UzAJFkav7hJZ2aF4qbszD");
+
+// This how to connect to or create a database.
+const db = deta.Base('sanpham');
+
+// You can create as many as you want without additional charges.
+
+export default {
+async mounted(){
+  let item = await db.get('hibtaia0p94v'); 
+  console.log(item)
+}
+}
+</script>
+
+<style>
+
+</style>
