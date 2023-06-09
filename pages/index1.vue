1df8926f98a88da023a23eb28dac0fafb6f4ede0
btm2021 <trinhminhbao@gmail.com>
2023-06-09 14:48:19 +0700

    Asd

--- pages/index1.vue
+++ pages/index1.vue
@@ -1,25 +1,51 @@
 <template>
   <div>
-bb
+    bb
   </div>
 </template>
 
 <script>
 
 export default {
-  layout: 'blog',
-  async mounted(){
-  
-    debugger
-  let result = await $pocketbase.collection('sanpham').getOne('rctyz63v0bb8r53', {
-    expand: 'hamluongvang.ten'
-});
- console.log(result)
 
-}
+  async mounted() {
+    //thiết lập realtime cho bảng giá
+    this.initRealtimeDB();
+
+  },
+  methods: {
+    initRealtimeDB(){
+      //bảng giá
+      this.$pocketbase.collection('banggia').subscribe('*',  (e) =>{
+      this.makeToast("Bảng giá thay đổi","success")
+    });
+    //hóa đơn
+    this.$pocketbase.collection('hoadon').subscribe('*',  (e) =>{
+      console.log(e)
+      let str=''
+  
+      if(e.action==="create"){
+        str=`[ĐƠN MỚI] - có 1 hóa đơn mới được tạo với mã số  #${e.record.collectionId}, nội dung chi tiết vui lòng theo link`
+        this.makeToast("Có hóa đơn mới","success")
+      }else if(e.action==='update '){
+        str=`[ĐỔI TRẢ] - hóa đơn #${e.record.collectionId} vừa đánh dấu là đổi trả, nội dung chi tiết vui lòng theo link ` 
+      }
+  
+      this.makeToast(str,'primary')
+    });
+    //tem
+    console.log('* thiết lập realtime SSE tới Server')
+    },
+    makeToast(msg, variant) {
+      this.$bvToast.toast(msg, {
+        title: 'Thông  báo',
+        autoHideDelay: 20000,
+        appendToast: true,
+        variant
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
